3bdae5fdeca3e9faf98d836c8790cc47e2aa441b
s0m3thln9 <dunlock67@gmail.com>
2025-06-26 12:05:52 +0300

    initial state

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+  ],
+})
