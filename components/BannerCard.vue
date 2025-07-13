1c12f6c1bbad6c11c21ef7bc553bfc61f951c350
s0m3thln9 <dunlock67@gmail.com>
2025-07-13 10:53:26 +0300

    add collection page with adaptivity

--- components/BannerCard.vue
+++ components/BannerCard.vue
@@ -2,6 +2,7 @@
 defineProps<{
 	imageUrl: string
 	text?: string
+	plus?: boolean
 	maxHeight?: string
 	customClass?: string
 	objectPosition?: string
@@ -26,9 +27,15 @@ defineProps<{
     />
     <div
 	    v-if="text"
-	    class="absolute bottom-2 right-2 px-2 py-1 bg-[#FFFFFA99] rounded-2xl font-[Manrope] text-[#211D1D] uppercase text-[10px] sm:text-base/5 sm:px-4 sm:py-2 sm:bottom-6 sm:right-6 sm:bg-[#FFFFFA]"
+	    class="absolute bottom-2 right-2 px-2 py-1 bg-[#FFFFFA99] rounded-2xl font-[Commissioner] font-light text-[#211D1D] uppercase text-[10px] sm:text-base/5 sm:font-[Manrope] sm:font-normal sm:px-4 sm:py-2 sm:bottom-6 sm:right-6 sm:bg-[#FFFFFA]"
     >
       {{ text }}
+    </div>
+	  <div
+		  v-if="plus"
+		  class="absolute bottom-2 right-2 px-1 py-1 bg-[#FFFFFA99] rounded-sm font-[Commissioner] font-light text-[#211D1D] uppercase text-[10px] sm:rounded-lg sm:text-base/5 sm:font-[Manrope] sm:font-normal sm:px-2 sm:bottom-6 sm:right-6"
+	  >
+      +
     </div>
   </div>
 </template>
---
+++ pages/collections/collection/index.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+const images = {
+	camp1: "/camp-1.jpg",
+	camp2: "/camp-2.jpg",
+	camp3: "/camp-3.jpg",
+	camp4: "/camp-4.jpg",
+	camp5: "/camp-5.jpg",
+	camp6: "/camp-6.jpg",
+	camp7: "/camp-7.jpg",
+	camp8: "/camp-8.jpg",
+	camp9: "/camp-9.jpg",
+	camp10: "/camp-10.jpg",
+	camp11: "/camp-11.jpg",
+	camp12: "/camp-12.jpg",
+	camp13: "/camp-13.jpg",
+	camp14: "/camp-14.jpg",
+}
+</script>
+
+<template>
+  <main class="mb-5 font-[Manrope] bg-[#FFFFFA] text-[#211D1D] sm:mb-10">
+	  <div class="p-2 sm:px-4 sm:py-6">
+		  <span
+			  class="text-[10px] font-light text-[#211D1D] font-[Commissioner] sm:font-[Manrope] sm:text-sm"
+		  >
+			 Главная / Сертификат
+		  </span>
+	  </div>
+		<h2 class="uppercase text-center font-[Inter] text-[17px]">Campaign</h2>
+	  <div class="grid grid-cols-2 mt-4 px-2 gap-2 sm:gap-4 sm:px-4 sm:mt-10 sm:grid-cols-4">
+		  <BannerCard
+			  :image-url="images.camp1"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp2"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp3"
+			  plus
+			  custom-class="rounded-lg aspect-[936/680] col-span-2"
+			  object-position="50% 70%"
+		  />
+		  <BannerCard
+			  :image-url="images.camp4"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp5"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp6"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp7"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp8"
+			  plus
+			  custom-class="rounded-lg aspect-[936/680] col-span-2"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp9"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp10"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp11"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp12"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp13"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+		  <BannerCard
+			  :image-url="images.camp14"
+			  plus
+			  custom-class="rounded-lg aspect-[460/680]"
+			  object-position="center"
+		  />
+	  </div>
+	</main>
+</template>
+
+<style scoped>
+
+</style>
