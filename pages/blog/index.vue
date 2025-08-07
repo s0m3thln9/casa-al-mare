67926885535a0a70310d195c92a3f7b5ada18dfb
s0m3thlin9 <dunlock67@gmail.com>
2025-08-07 22:52:03 +0300

    refactoring of blog page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -7,7 +7,35 @@ const images = {
 	blog5: "/blog-5.jpg",
 	blog6: "/blog-6.jpg",
 }
+
+const areImagesLoaded = ref(false)
+
+const blogImages = [images.blog1, images.blog2, images.blog3, images.blog4, images.blog5, images.blog6]
+
 const categories = ["Все", "Уход", "Стиль", "Советы"]
+
+const preloadImages = async () => {
+	const loadImage = (url: string) => {
+		return new Promise((resolve, reject) => {
+			const img = new Image()
+			img.src = url
+			img.onload = resolve
+			img.onerror = reject
+		})
+	}
+	
+	try {
+		await Promise.all(blogImages.map(url => loadImage(url)))
+		areImagesLoaded.value = true
+	} catch (error) {
+		console.error('Ошибка при загрузке изображений:', error)
+		areImagesLoaded.value = true
+	}
+}
+
+onMounted(() => {
+	preloadImages()
+})
 
 </script>
 
@@ -15,32 +43,44 @@ const categories = ["Все", "Уход", "Стиль", "Советы"]
    <main class="mb-5 font-[Manrope] bg-[#FFFFFA] text-[#211D1D] sm:mb-10">
 	   <h2 class="uppercase text-center font-[Inter] text-[17px]">Блог</h2>
 	   <div class="mt-4 w-full flex justify-center items-center px-2">
-		   <SelectButton :variants="categories" custom-class="max-w-[600px] w-full " custom-button-class="flex-1" />
+		   <SelectButton :variants="categories" custom-class="max-w-[600px] w-full" custom-button-class="flex-1" />
 	   </div>
-	   <div class="grid px-2 mt-4 sm:mt-10 sm:px-4 sm:gap-6 lg:grid-cols-3">
+	   <div class="grid px-2 mt-4 gap-6 sm:mt-10 sm:px-4 lg:grid-cols-3">
 		   <div class="flex flex-col items-center cursor-pointer" @click="navigateTo('/blog/article')">
-			   <NuxtImg :src="images.blog1" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
-			   <h3 class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Краткое название статьи</h3>
+			   <div v-if="!areImagesLoaded" class="aspect-[610/570] w-full bg-[#F9F6EC] rounded-lg" />
+			   <NuxtImg v-else :src="images.blog1" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
+			   <div v-if="!areImagesLoaded" class="h-10 w-full bg-[#F9F6EC] rounded-lg mt-2 sm:mt-4" />
+			   <h3 v-else class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Краткое название статьи</h3>
 		   </div>
 		   <div class="flex flex-col items-center cursor-pointer" @click="navigateTo('/blog/article')">
-			   <NuxtImg :src="images.blog2" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
-			   <h3 class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Очень длинное название статьи о трендах в путешествиях ...</h3>
+			   <div v-if="!areImagesLoaded" class="aspect-[610/570] w-full bg-[#F9F6EC] rounded-lg" />
+			   <NuxtImg v-else :src="images.blog2" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
+			   <div v-if="!areImagesLoaded" class="h-10 w-full bg-[#F9F6EC] rounded-lg mt-2 sm:mt-4" />
+			   <h3 v-else class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Очень длинное название статьи о трендах в путешествиях ...</h3>
 		   </div>
 		   <div class="flex flex-col items-center cursor-pointer" @click="navigateTo('/blog/article')">
-			   <NuxtImg :src="images.blog3" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
-			   <h3 class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Среднее по длине название статьи</h3>
+			   <div v-if="!areImagesLoaded" class="aspect-[610/570] w-full bg-[#F9F6EC] rounded-lg" />
+			   <NuxtImg v-else :src="images.blog3" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
+			   <div v-if="!areImagesLoaded" class="h-10 w-full bg-[#F9F6EC] rounded-lg mt-2 sm:mt-4" />
+			   <h3 v-else class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Среднее по длине название статьи</h3>
 		   </div>
 		   <div class="flex flex-col items-center cursor-pointer" @click="navigateTo('/blog/article')">
-			   <NuxtImg :src="images.blog4" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
-			   <h3 class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Очень длинное название статьи о трендах в путешествиях ...</h3>
+			   <div v-if="!areImagesLoaded" class="aspect-[610/570] w-full bg-[#F9F6EC] rounded-lg" />
+			   <NuxtImg v-else :src="images.blog4" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
+			   <div v-if="!areImagesLoaded" class="h-10 w-full bg-[#F9F6EC] rounded-lg mt-2 sm:mt-4" />
+			   <h3 v-else class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Очень длинное название статьи о трендах в путешествиях ...</h3>
 		   </div>
 		   <div class="flex flex-col items-center cursor-pointer" @click="navigateTo('/blog/article')">
-			   <NuxtImg :src="images.blog5" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
-			   <h3 class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Краткое название статьи</h3>
+			   <div v-if="!areImagesLoaded" class="aspect-[610/570] w-full bg-[#F9F6EC] rounded-lg" />
+			   <NuxtImg v-else :src="images.blog5" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
+			   <div v-if="!areImagesLoaded" class="h-10 w-full bg-[#F9F6EC] rounded-lg mt-2 sm:mt-4" />
+			   <h3 v-else class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Краткое название статьи</h3>
 		   </div>
 		   <div class="flex flex-col items-center cursor-pointer" @click="navigateTo('/blog/article')">
-			   <NuxtImg :src="images.blog6" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
-			   <h3 class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Среднее по длине название статьи</h3>
+			   <div v-if="!areImagesLoaded" class="aspect-[610/570] w-full bg-[#F9F6EC] rounded-lg" />
+			   <NuxtImg v-else :src="images.blog6" alt="blog" width="610" height="570" class="aspect-[610/570] object-cover rounded-lg" />
+			   <div v-if="!areImagesLoaded" class="h-10 w-full bg-[#F9F6EC] rounded-lg mt-2 sm:mt-4" />
+			   <h3 v-else class="uppercase text-center mt-2 font-[Manrope] text-[13px] max-w-[610px] sm:font-[Inter] sm:text-xl sm:mt-4">Среднее по длине название статьи</h3>
 		   </div>
 	   </div>
    </main>
