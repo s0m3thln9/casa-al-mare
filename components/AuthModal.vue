415c47be1612627f989f5ae92434efe0e257ca74
s0m3thln9 <dunlock67@gmail.com>
2025-09-17 11:29:06 +0300

    start refactoring authModal

--- components/AuthModal.vue
+++ components/AuthModal.vue
@@ -1,41 +1,101 @@
 <script setup lang="ts">
-import {SelectInput} from "#components"
-
-const phone = ref('')
-
 const authStore = useAuthStore()
 const authModalStore = useAuthModalStore()
 
-const selectInputRef = ref<InstanceType<typeof SelectInput>>()
+const selectInputRef = ref()
 
 const handleClick = () => {
-	if (!selectInputRef.value?.validateInput()) return
-	authStore.login()
-	authModalStore.close()
+  if (!selectInputRef.value?.validate()) return
+  authStore.login()
+  authModalStore.close()
 }
-
 </script>
 
 <template>
   <div class="absolute inset-0 z-10 flex justify-center items-center">
-	  <div class="w-[300px] bg-[#FFFFFA] border-[0.7px] border-[#211D1D] rounded-2xl">
-		  <div class="flex justify-end items-center">
-			  <button
-				  class="p-4 cursor-pointer"
-				  @click="authModalStore.close"
-			  >
-				  <NuxtImg src="/x.svg" width="24" />
-			  </button>
-		  </div>
-		  <div class="px-6 pb-6">
-			  <h2 class="mb-8 font-[Inter] text-2xl text-[#211D1D] text-center">Авторизуйтесь, чтобы сохранить избранное</h2>
-			  <SelectInput ref="selectInputRef" id="phone" v-model="phone" :options="[  { code: '+7', country: 'Россия' }, { code: '+375', country: 'Беларусь' }, { code: '+380', country: 'Украина' }, { code: '+77', country: 'Казахстан' }, { code: '+998', country: 'Узбекистан' }, { code: '+992', country: 'Таджикистан' }, { code: '+993', country: 'Туркменистан' }, { code: '+996', country: 'Кыргызстан' }, { code: '+374', country: 'Армения' }, { code: '+994', country: 'Азербайджан' }, { code: '+373', country: 'Молдова' }, { code: '+7', country: 'Абхазия' }, { code: '+995', country: 'Грузия' }]" type="text" label="Номер телефона" required />
-			  <AppButton variant="primary" content="Войти" custom-class="w-full mt-4" @click="handleClick" />
-		  </div>
-	  </div>
+    <div class="w-[300px] bg-[#FFFFFA] border-[0.7px] border-[#211D1D] rounded-2xl">
+      <div class="flex justify-end items-center">
+        <button
+          class="p-2 cursor-pointer"
+          @click="authModalStore.close"
+        >
+          <NuxtImg
+            src="/x.svg"
+            width="24"
+          />
+        </button>
+      </div>
+      <div class="px-6 pb-6 flex flex-col items-center">
+        <h2 class="mb-8 font-[Manrope] text-sm text-[#211D1D] text-center uppercase">Присоединиться</h2>
+        <SelectButton
+          v-model="authStore.method"
+          :variants="['По телефону', 'По почте']"
+          custom-class="mb-8 w-fit"
+        />
+        <template v-if="authStore.method === 'По телефону'">
+          <SelectInput
+            id="phone"
+            ref="selectInputRef"
+            v-model="authStore.phone"
+            :options="[
+              { code: '+7', country: 'Россия' },
+              { code: '+375', country: 'Беларусь' },
+              { code: '+380', country: 'Украина' },
+              { code: '+77', country: 'Казахстан' },
+              { code: '+998', country: 'Узбекистан' },
+              { code: '+992', country: 'Таджикистан' },
+              { code: '+993', country: 'Туркменистан' },
+              { code: '+996', country: 'Кыргызстан' },
+              { code: '+374', country: 'Армения' },
+              { code: '+994', country: 'Азербайджан' },
+              { code: '+373', country: 'Молдова' },
+              { code: '+7', country: 'Абхазия' },
+              { code: '+995', country: 'Грузия' },
+            ]"
+            type="text"
+            label="Номер телефона"
+            required
+            custom-class="w-full"
+          />
+          <AppButton
+            variant="primary"
+            :content="authStore.phoneButtonState.content"
+            :disabled="authStore.phoneButtonState.disabled"
+            custom-class="w-full mt-4"
+            @click="handleClick"
+          />
+          <span class="font-[Manrope] text-xs font-light text-[#5E5B58] mt-8 cursor-pointer"
+            >Нет аккаунта? Регистрация</span
+          >
+        </template>
+        <template v-else>
+          <AppInput
+            id="email"
+            v-model="authStore.email"
+            type="email"
+            label="Почта"
+            required
+            custom-class="w-full"
+          />
+          <AppInput
+            id="password"
+            v-model="authStore.password"
+            type="password"
+            label="Пароль"
+            required
+            custom-class="w-full mt-4"
+          />
+          <AppButton
+            variant="primary"
+            :content="authStore.emailButtonState.content"
+            :disabled="authStore.emailButtonState.disabled"
+            custom-class="w-full mt-4"
+            @click="handleClick"
+          />
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- stores/auth.ts
+++ stores/auth.ts
@@ -1,12 +1,26 @@
-export const useAuthStore = defineStore('auth', () => {
-	const isAuth = ref(false)
-	
-	const login = () => {
-		isAuth.value = true
-	}
-	const logout = () => {
-		isAuth.value = false
-	}
-	
-	return { isAuth, login, logout  }
+export const useAuthStore = defineStore("auth", () => {
+  const isAuth = ref(false)
+  const method = ref("По телефону")
+  const phone = ref("")
+  const email = ref("")
+  const password = ref("")
+
+  const login = () => {
+    isAuth.value = true
+  }
+  const logout = () => {
+    isAuth.value = false
+  }
+
+  const phoneButtonState = computed(() => ({
+    content: method.value === "По телефону" ? "Отправить СМС-код" : "Войти",
+    disabled: !phone.value,
+  }))
+
+  const emailButtonState = computed(() => ({
+    content: method.value === "По почте" ? "Войти" : "Войти",
+    disabled: !email.value,
+  }))
+
+  return { isAuth, method, phone, email, password, phoneButtonState, emailButtonState, login, logout }
 })
