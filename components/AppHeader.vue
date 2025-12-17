d2ea4b46bf0c0ea0c365fc5456fcecea7a6d41b4
s0m3thln9 <dunlock67@gmail.com>
2025-12-17 21:56:50 +0300

    header fix

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -72,7 +72,7 @@ const handleMenuButtonClick = () => {
             Меню
           </button>
         </li>
-        <li class="hover:text-[#F3A454]"><NuxtLink to="/catalog">Новинки</NuxtLink></li>
+<!--        <li class="hover:text-[#F3A454]"><NuxtLink to="/catalog">Новинки</NuxtLink></li>-->
         <li class="hover:text-[#F3A454]"><NuxtLink to="/blog">Блог</NuxtLink></li>
         <li class="hover:text-[#F3A454]"><NuxtLink to="/campaigns">CAMPAIGNS
         </NuxtLink></li>
